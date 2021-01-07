7b1d4b5135fcf2ec89419569add62a3f35143532
Jie Zhou (JAY) <33584805+jayzhou125@users.noreply.github.com>
2021-01-07 17:52:30 -0500

    Update Cut off rank.py

--- Cut off rank.py
+++ Cut off rank.py
@@ -1,25 +1,37 @@
-def countLevelUpPlayers(cutOffRank, num, scores):                    
-    sortedS = sorted(scores, reverse = 1)
-    rank = []
-    r = 0
+# naive approach, TLE
+# def countLevelUpPlayers(cutOffRank, num, scores):                    
+#     sortedS = sorted(scores, reverse = 1)
+#     rank = []
+#     r = 0
     
-    for i in range(len(sortedS)):
-        if i == 0:
-            r += 1
-            rank.append(r)
-        else:
-            if sortedS[i] == sortedS[i-1]:
-                rank.append(r)
-            else:
-                r = i+1
-                rank.append(r)
+#     for i in range(len(sortedS)):
+#         if i == 0:
+#             r += 1
+#             rank.append(r)
+#         else:
+#             if sortedS[i] == sortedS[i-1]:
+#                 rank.append(r)
+#             else:
+#                 r = i+1
+#                 rank.append(r)
     
+#     count = 0
+#     for j in rank:
+#         if j <= cutOffRank:
+#             count+=1
+#         else:
+#             break
+    
+#     # return [rank, count]
+#     return count
+
+# all pass
+from collections import Counter
+def countLevelUpPlayers(cutOffRank, num, scores):
+    sortedS = sorted(list(set(scores)), reverse = 1)
+    scoreCount = Counter(scores)
     count = 0
-    for j in rank:
-        if j <= cutOffRank:
-            count+=1
-        else:
-            break
-    
-    # return [rank, count]
+    while count < cutOffRank:
+        score = sortedS.pop(0)
+        count += scoreCount[score]
     return count
