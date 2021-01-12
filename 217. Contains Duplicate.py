b32d0f2dacdd4f1dacb4fc8e0d492e1a00aca986
Jie Zhou (JAY) <33584805+jayzhou125@users.noreply.github.com>
2021-01-11 22:01:10 -0500

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,33 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # TLE 
+        # nums_length = len(nums)
+        # count = 0
+        # temp = []
+        # for i in nums:
+        #     if i not in temp:
+        #         temp.append(i)
+        #         count += 1
+        # if count != nums_length:
+        #     return True
+        # else:
+        #     return False
+        
+        # sort, steady performance
+#         if len(nums) == 1: return False
+#         if len(set(nums)) == 1: return True
+        
+#         nums.sort()
+#         print(nums)
+#         for i in range(len(nums)-1):
+#             print(nums[i], nums[i+1])
+#             if nums[i] == nums[i+1]:
+#                 return True
+#         return False
+        
+        # python use set
+        num_set = set(nums)
+        if len(num_set) != len(nums):
+            return True
+        else:
+            return False
