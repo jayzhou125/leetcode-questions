f5a9e338b0546e0f6beaf5c7d607435f4797fced
Jie Zhou (JAY) <33584805+jayzhou125@users.noreply.github.com>
2021-04-14 23:08:18 -0400

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -12,4 +12,17 @@ class Solution:
             hmap[nums[i]] = i
         return []
         
-            
+# Java
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++){
+            int complement = target - nums[i];
+            if (hm.containsKey(complement)){
+                return new int[] {hm.get(complement), i};
+            }
+            hm.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum sulotion");
+    }
+}
